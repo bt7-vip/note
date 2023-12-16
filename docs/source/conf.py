aabce02aa60db4c137653c96378a5e4b5edc7ea6
HJG <vyun@live.com>
2023-12-16 13:19:20 +0800

    up conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'groundwork'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'groundwork' 这个主题好像不支持md文件
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
